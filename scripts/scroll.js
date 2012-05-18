7279c465b17166d404f8016589554efd810c0452
Julien Dubreuil <dubreuil.julien@gmail.com>
2012-05-17 23:06:57 -0700

    Updated scroll to the top js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,1 +1,34 @@
-jQuery.fn.topLink=function(a){a=jQuery.extend({min:1,fadeSpeed:200},a);return this.each(function(){var b=$(this);b.hide();$(window).scroll(function(){if($(window).scrollTop()>=a.min){b.fadeIn(a.fadeSpeed)}else{b.fadeOut(a.fadeSpeed)}})})};(function(){$(document).ready(function(){$("#top-link").topLink({min:400,fadeSpeed:400});$("#top-link").on("click",function(a){$("html, body").animate({scrollTop:0});return false});$(".navlist a").on("click",function(a){a.preventDefault();var b=$(this).attr("href");var c=$(b).offset().top;$("html, body").animate({scrollTop:c},function(){window.location.hash=b});return false})})})()
+jQuery.fn.topLink = function () {
+    return this.each(function () {
+        var elem  = $(this)
+            speed = 400;
+        elem.hide();
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >= 1) {
+                elem.slideDown(speed)
+            } else {
+                elem.slideUp(speed)
+            }
+        })
+    })
+};
+(function () {
+    $(document).ready(function () {    
+        $("#toTheTop").topLink();
+        $("#toTheTop").on("click", function () {
+            return $("html, body").animate({ scrollTop: 0 });            
+        });
+
+        $(".navlist a").on("click", function (a) {
+            a.preventDefault();
+            var attr   = $(this).attr("href"),
+                offset = $(attr).offset().top;
+            $("html, body").animate({
+                scrollTop: offset
+            }, function () {
+                window.location.hash = attr
+            });
+            return false
+        })
+    })
+})()
